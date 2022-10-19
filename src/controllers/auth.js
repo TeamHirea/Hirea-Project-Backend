36bc228120d41f681815d0fda3bdbcd2915b4c54
graciasvito <vitoristo1@gmail.com>
2022-10-19 16:17:21 +0700

    feat: send email not complete

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,9 +1,12 @@
-// const random = require("simple-random-number-generator");
+const random = require("simple-random-number-generator");
 
 const bcrypt = require("bcrypt");
 const authModel = require("../models/auth");
 const wrapper = require("../utils/responseHandler");
 const userModel = require("../models/user");
+const client = require("../config/redis");
+
+const { sendEmail } = require("../utils/nodemailer");
 
 module.exports = {
   signupRecruiter: async (request, response) => {
@@ -55,22 +58,78 @@ module.exports = {
 
       // save data by model
       await userModel.createRecruiter(setData);
+      // get data user
       const getDataUser = await authModel.getUserByEmail(email);
       delete getDataUser.data[0].password;
 
-      // const otp = {
-      //   min: 100000,
-      //   max: 999999,
-      //   integer: true,
-      // };
+      const userId = getDataUser.data[0].id;
+      const otp = random({
+        min: 100000,
+        max: 999999,
+        integer: true,
+      });
+
+      // mailing
+      const setMailOptions = {
+        email,
+        title: "Hirea Apps",
+        greeting: "Hello",
+        name,
+        subject: "Email Verification !",
+        subtitle: "Email Verification",
+        message: "Please confirm your OTP by clicking the link",
+        otp,
+        template: "template-1.html",
+        button: `http://localhost:8080/api/auth/verify/${otp}`,
+      };
+
+      await sendEmail(setMailOptions);
+
+      // save OTP in redis
+      client.setEx(`otp:${otp}`, 3600, userId);
 
       return wrapper.response(
         response,
         200,
         "Success Register Please Check Your Email",
-        getDataUser.data
+        { id: userId }
       );
     } catch (error) {
+      console.log(error);
+      const {
+        status = 500,
+        statusText = "Internal Server Error",
+        error: errorData = null,
+      } = error;
+      return wrapper.response(response, status, statusText, errorData);
+    }
+  },
+  verify: async (request, response) => {
+    try {
+      const { otp } = request.params;
+
+      const checkOTP = await client.get(`otp:${otp}`);
+      const today = new Date().toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta",
+      });
+      if (!checkOTP) {
+        return wrapper.response(response, 403, "Wrong OTP", null);
+      }
+
+      const setData = {
+        activatedAt: today,
+      };
+      const result = await userModel.updateUser(checkOTP, setData);
+
+      client.del(`otp:${otp}`);
+      return wrapper.response(
+        response,
+        result.status,
+        "Success Verified, Please Login",
+        { userId: checkOTP }
+      );
+    } catch (error) {
+      // console.log(error);
       const {
         status = 500,
         statusText = "Internal Server Error",
