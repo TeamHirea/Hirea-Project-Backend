7098feb7b62400d286d014a174607f5b338d2878
Ridho Ahmad Batubara <ridho.beteer@gmail.com>
2022-10-21 18:51:44 +0700

    feat: add create and get experience

--- src/routes/experience.js
+++ src/routes/experience.js
@@ -5,5 +5,7 @@ const experienceController = require("../controllers/experience");
 
 // update experience
 Router.patch("/:id", experienceController.updateJobSeekerExperience);
+Router.post("/", experienceController.createJobSeekerExperience);
+Router.get("/:id", experienceController.getJobSeekerExperienceById);
 
 module.exports = Router;
